fc633ba744eac5b166fa73095d6671891548738f
Devender Singh <dsingh@jamesriverins.com>
2019-09-10 14:06:07 +0530

    updated - option list reseting after field change

--- DataEntryApp/UserControls/TemplateFieldControls/TemplateList.ascx.cs
+++ DataEntryApp/UserControls/TemplateFieldControls/TemplateList.ascx.cs
@@ -193,9 +193,14 @@ namespace TechTicket.DataEntry.UserControls
         protected void Select_RadioButton(object sender, DirectEventArgs e)
         {
 
+
             if (rdHidden.Checked)
                 return;
 
+            Session["FieldOptions"] = null;
+            cbxFieldOptions.Reset();
+            //cbxFieldOptions.remove;
+            strFieldOptions.RemoveAll();
             int optionListOffset = 0, saveTemplateOffset = 0;
             if (Session["FieldList"] != null)
             {
